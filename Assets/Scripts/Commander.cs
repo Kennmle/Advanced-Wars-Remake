398edb999e179ba5cdfde3972e8524efabbe8d3b
divinedude <kennethandrewle@gmail.com>
2017-07-06 11:33:39 -0400

    Fuck u justin
    LMAO

--- Assets/Scripts/Commander.cs
+++ Assets/Scripts/Commander.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public abstract class Commander : MonoBehaviour {
 
 
-    private String name;
+    private String co;
     private int cozone;
     private int copower;
-    private ImageIcon img;
+
+    //private ImageIcon img; motherfucker
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
