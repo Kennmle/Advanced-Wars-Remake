5a6c8d7e606619b1a7fbf3e6a02066716be67b0a
Kennmle <kennmle@gmail.com>
2018-01-02 15:45:40 -0500

    Start of BattleSystem dictionaries
    WHEE

--- Advance-Wars-Remake/Assets/Scripts/Units/BattleSystem.cs
+++ Advance-Wars-Remake/Assets/Scripts/Units/BattleSystem.cs
@@ -21,8 +21,63 @@ public class BattleSystem : MonoBehaviour {
 			unit1.getFuel();
 		}
 	}
-	private void dictInfantry() {
-		Dictionary<Type, int> dict = new Dictionary<Type, int>();
-		dict.Add(typeof(TestUnit), 69);
+	private void dictInit() {
+		Dictionary dict = new Dictionary<Type, int>();
+		dict.Add(typeof(Infantry),55);
+		dict.Add(typeof(Mech),45);
+		dict.Add(typeof(Bike),45);
+		dict.Add(typeof(Recon),12);
+		dict.Add(typeof(Flare),10);
+		dict.Add(typeof(AntiAir),3);
+		dict.Add(typeof(Tank),5);
+		dict.Add(typeof(MediumTank),5);
+		dict.Add(typeof(WarTank),1);
+		dict.Add(typeof(Artillery),10);
+		dict.Add(typeof(AntiTank),30);
+		dict.Add(typeof(Rocket),20);
+		dict.Add(typeof(Missile),20);
+		dict.Add(typeof(Rig),14);
+		dict.Add(typeof(Battleship),0);
+		dict.Add(typeof(Carrier),0);
+		dict.Add(typeof(Submarine),0);
+		dict.Add(typeof(Cruiser),0);
+		dict.Add(typeof(GunBoat),0);
+		dict.Add(typeof(Lander),0);
+		dict.Add(typeof(Duster),0);
+		dict.Add(typeof(Bomber),0);
+		dict.Add(typeof(Fighter),0);
+		dict.Add(typeof(BCopter),8);
+		dict.Add(typeof(TCopter),30);
+		dict.Add(typeof(SeaPlane),0);
+		battle.Add(typeof(Infantry),dict);
+
+		dict = new Dictionary<Type, int>();
+		dict.Add(typeof(Infantry),65);
+		dict.Add(typeof(Mech),55);
+		dict.Add(typeof(Bike),55);
+		dict.Add(typeof(Recon),85);
+		dict.Add(typeof(Flare),80);
+		dict.Add(typeof(AntiAir),55);
+		dict.Add(typeof(Tank),55);
+		dict.Add(typeof(MediumTank),25);
+		dict.Add(typeof(WarTank),15);
+		dict.Add(typeof(Artillery),70);
+		dict.Add(typeof(AntiTank),55);
+		dict.Add(typeof(Rocket),85);
+		dict.Add(typeof(Missile),85);
+		dict.Add(typeof(Rig),75);
+		dict.Add(typeof(Battleship),0);
+		dict.Add(typeof(Carrier),0);
+		dict.Add(typeof(Submarine),0);
+		dict.Add(typeof(Cruiser),0);
+		dict.Add(typeof(GunBoat),0);
+		dict.Add(typeof(Lander),0);
+		dict.Add(typeof(Duster),0);
+		dict.Add(typeof(Bomber),0);
+		dict.Add(typeof(Fighter),0);
+		dict.Add(typeof(BCopter),12);
+		dict.Add(typeof(TCopter),35);
+		dict.Add(typeof(SeaPlane),0);
+		battle.Add(typeof(Mech),dict);
 	}
 }
